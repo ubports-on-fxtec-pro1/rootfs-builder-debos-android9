6e06492df0a9cbdbd5f9aa48776acf3794f3131b
Marius Gripsgard <marius@ubports.com>
2019-08-28 03:32:37 +0200

    Ping repos

--- scripts/add-mainline-repos.sh
+++ scripts/add-mainline-repos.sh
@@ -7,8 +7,20 @@ echo "nameserver 1.1.1.1" > /etc/resolv.conf
 echo "deb http://repo.ubports.com/ xenial_-_edge_-_mesa main" >> /etc/apt/sources.list.d/ubports.list
 echo "deb http://repo.ubports.com/ xenial_-_edge_-_pine main" >> /etc/apt/sources.list.d/ubports.list
 
+echo "" >> /etc/apt/preferences.d/ubports.pref
+echo "Package: *" >> /etc/apt/preferences.d/ubports.pref
+echo "Pin: origin repo.ubports.com" >> /etc/apt/preferences.d/ubports.pref
+echo "Pin: release o=UBports,a=xenial_-_edge_-_mesa" >> /etc/apt/preferences.d/ubports.pref
+echo "Pin-Priority: 2000" >> /etc/apt/preferences.d/ubports.pref
+
+echo "" >> /etc/apt/preferences.d/ubports.pref
+echo "Package: *" >> /etc/apt/preferences.d/ubports.pref
+echo "Pin: origin repo.ubports.com" >> /etc/apt/preferences.d/ubports.pref
+echo "Pin: release o=UBports,a=xenial_-_edge_-_pine" >> /etc/apt/preferences.d/ubports.pref
+echo "Pin-Priority: 2000" >> /etc/apt/preferences.d/ubports.pref
+
 apt update
-apt upgrade -y
+apt upgrade -y --allow-downgrades
 apt autoremove -y
 
 apt install ubuntu-touch-session-wayland -y
