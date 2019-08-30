e827ea71316ac1936f9a36690e5142a6d8c57e1d
Marius Gripsgard <marius@ubports.com>
2019-08-30 15:47:22 +0200

    Also shave off -dbg and unsigned- from kernel version

--- scripts/try-depmod-installed.sh
+++ scripts/try-depmod-installed.sh
@@ -3,6 +3,8 @@
 # This gets the last kernel that gets listed from dpkg. *should be latest*
 kern=$(dpkg -l | grep linux-image- | tail -n1 | awk '{print $2}')
 kver=${kern/linux-image-/}
+kver=${kver/-dbg/}
+kver=${kver/unsigned-/}
 
 echo "Found: $kver"
 depmod -a $kver
