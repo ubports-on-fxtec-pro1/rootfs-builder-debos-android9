e7b938fd61a2b655c57ebad7c5d98f37651d5892
Marius Gripsgard <marius@ubports.com>
2019-09-02 17:31:28 +0200

    fsck and force tune2fs

--- scripts/no-metadatacsum.sh
+++ scripts/no-metadatacsum.sh
@@ -4,5 +4,6 @@ MOUNTPATH=$(realpath mnt/$1)
 PART=$(df "$MOUNTPATH" | tail -1 | awk '{ print $1 }')
 
 umount -l $PART
-tune2fs -O ^metadata_csum $PART
+e2fsck -f $PART
+tune2fs -f -O ^metadata_csum $PART
 mount $PART $MOUNTPATH
