e5fa42f7b883b8f08987365e0937c4e7d934a9a1
Marius Gripsgard <marius@ubports.com>
2019-09-02 18:06:28 +0200

    Use a slighly less hacky method to disable metadata_csum

--- scripts/no-metadatacsum.sh
+++ scripts/no-metadatacsum.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-MOUNTPATH=$(realpath mnt/$1)
-PART=$(df "$MOUNTPATH" | tail -1 | awk '{ print $1 }')
-
-umount -l $PART
-e2fsck -f $PART
-tune2fs -f -O ^metadata_csum $PART
-mount $PART $MOUNTPATH
+sed -i 's/,metadata_csum//g' /etc/mke2fs.conf
