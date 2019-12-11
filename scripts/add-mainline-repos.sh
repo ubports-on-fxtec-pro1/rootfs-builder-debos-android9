c491f33147f20220bc4461ac98d4022c3a80f673
Marius Gripsgard <marius@ubports.com>
2019-12-11 12:46:36 +0100

    Add wayland branch

--- scripts/add-mainline-repos.sh
+++ scripts/add-mainline-repos.sh
@@ -9,6 +9,7 @@ export DEBCONF_NONINTERACTIVE_SEEN=true
 
 echo "deb http://repo.ubports.com/ xenial_-_edge_-_mesa main" >> /etc/apt/sources.list.d/ubports.list
 echo "deb http://repo.ubports.com/ xenial_-_edge_-_pine main" >> /etc/apt/sources.list.d/ubports.list
+echo "deb http://repo.ubports.com/ xenial_-_edge_-_wayland main" >> /etc/apt/sources.list.d/ubports.list
 
 echo "" >> /etc/apt/preferences.d/ubports.pref
 echo "Package: *" >> /etc/apt/preferences.d/ubports.pref
@@ -20,6 +21,12 @@ echo "" >> /etc/apt/preferences.d/ubports.pref
 echo "Package: *" >> /etc/apt/preferences.d/ubports.pref
 echo "Pin: origin repo.ubports.com" >> /etc/apt/preferences.d/ubports.pref
 echo "Pin: release o=UBports,a=xenial_-_edge_-_pine" >> /etc/apt/preferences.d/ubports.pref
+echo "Pin-Priority: 2002" >> /etc/apt/preferences.d/ubports.pref
+
+echo "" >> /etc/apt/preferences.d/ubports.pref
+echo "Package: *" >> /etc/apt/preferences.d/ubports.pref
+echo "Pin: origin repo.ubports.com" >> /etc/apt/preferences.d/ubports.pref
+echo "Pin: release o=UBports,a=xenial_-_edge_-_wayland" >> /etc/apt/preferences.d/ubports.pref
 echo "Pin-Priority: 2002" >> /etc/apt/preferences.d/ubports.pref
 
 apt update
