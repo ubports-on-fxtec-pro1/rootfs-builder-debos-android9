3ad5caa5da5d24538d23ba25a37f10b1b58d5210
NeKit <nekit1000@gmail.com>
2020-03-05 09:35:11 +0100

    android9: add missing script

---
+++ scripts/add-android8-repos.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Temporary set up the nameserver
+mv /etc/resolv.conf /etc/resolv2.conf
+echo "nameserver 1.1.1.1" > /etc/resolv.conf
+
+export DEBIAN_FRONTEND=noninteractive
+export DEBCONF_NONINTERACTIVE_SEEN=true
+
+echo "deb http://repo.ubports.com/ xenial_-_edge_-_android8 main" >> /etc/apt/sources.list.d/ubports.list
+
+echo "" >> /etc/apt/preferences.d/ubports.pref
+echo "Package: *" >> /etc/apt/preferences.d/ubports.pref
+echo "Pin: origin repo.ubports.com" >> /etc/apt/preferences.d/ubports.pref
+echo "Pin: release o=UBports,a=xenial_-_edge_-_android8" >> /etc/apt/preferences.d/ubports.pref
+echo "Pin-Priority: 2000" >> /etc/apt/preferences.d/ubports.pref
+
+apt update
+apt upgrade -y --allow-downgrades
+
+# Restore symlink
+rm /etc/resolv.conf
+mv /etc/resolv2.conf /etc/resolv.conf
