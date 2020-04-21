f8ab4588e98d7fca34b708b4ac90a0cccc2f2e63
Dalton Durst <dalton@ubports.com>
2020-04-21 13:39:51 -0500

    Clean up a bit
    Remove unused uboot-compile.sh script and make the resolver failure
    workaround more clear.

--- scripts/wget-dpkg.sh
+++ scripts/wget-dpkg.sh
@@ -2,7 +2,7 @@
 
 # This is a hack to fix the damn network problems with debos/fakemachine
 
-# Hack my damn network!
+# Work around resolver failure in debos' fakemachine
 mv /etc/resolv.conf /etc/resolv2.conf
 echo "nameserver 1.1.1.1" > /etc/resolv.conf
 
@@ -12,6 +12,6 @@ wget $1
 dpkg -i *.deb
 rm -r /var/tmp/inst
 
-# Bring back my damn network
+# Undo changes to work around debos fakemachine resolver
 rm /etc/resolv.conf
 mv /etc/resolv2.conf /etc/resolv.conf
